da5311b8df854035dbff700fab2b4aa41fef4222
Abhinav M <abhinavm0489@gmail.com>
2026-06-19 16:18:28 +0530

    fix api url

--- src/studentService.js
+++ src/studentService.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const API = 'https://thomas.proxy.rlwy.net:19023/api/students';
-
 export const getAllStudents  = ()              => axios.get(API);
 export const createStudent  = (student)       => axios.post(API, student);
 export const updateStudent  = (id, student)   => axios.put(`${API}/${id}`, student);
