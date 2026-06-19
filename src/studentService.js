ca30a9664caab5de48a24f0bafef5ce503396dcc
Abhinav M <abhinavm0489@gmail.com>
2026-06-19 18:01:08 +0530

    fix frontend api url

--- src/studentService.js
+++ src/studentService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = 'https://thomas.proxy.rlwy.net:19023/api/students';
+const API = 'https://studentmanagement-production-fbe6.up.railway.app/api/students';
 export const getAllStudents  = ()              => axios.get(API);
 export const createStudent  = (student)       => axios.post(API, student);
 export const updateStudent  = (id, student)   => axios.put(`${API}/${id}`, student);
