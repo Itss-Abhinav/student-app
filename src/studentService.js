6a9378f2140d0726157154db50e2445800e2fc1e
Abhinav M <abhinavm0489@gmail.com>
2026-06-19 19:00:10 +0530

    update railway domain

--- src/studentService.js
+++ src/studentService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = 'https://studentmanagement-production-fbe6.up.railway.app/api/students';
+const API = 'https://studentmanagement-production-f1c6.up.railway.app/api/students';
 export const getAllStudents  = ()              => axios.get(API);
 export const createStudent  = (student)       => axios.post(API, student);
 export const updateStudent  = (id, student)   => axios.put(`${API}/${id}`, student);
